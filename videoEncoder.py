471d40d025204d890c7c76df1d69280081c921f3
Adar Guy <adarguy10@gmail.com>
2017-05-12 12:02:23 -0700

    added no-name functionality, and dynamic sequence length

--- videoEncoder.py
+++ videoEncoder.py
@@ -5,20 +5,27 @@ print ("Welcome to Byte Camp's Video Encoder.\n")
 filepath = raw_input("Drag-n-Drop the first JPG or PNG image ending in '0000.jpg' or similar\nAnd press ENTER to make your video!\n")
 if (filepath[0] == "\"" and filepath[-1] == "\""):
 	filepath = filepath[1:-1]
-print (filepath)
 
-startNumStr = filepath[-8:-4]
-startNum = str(int(startNumStr))
+seq = filepath[-5]; c = 0; i = 1;
+while seq.isdigit():
+	c += 1
+	try:
+		seq = filepath[-5-c]
+	except Exception:
+		filepath = filepath[:-4-c]+"student"+filepath[-4-c:]
 
-if (startNum > -1):
+try:
+	startNum = int(filepath[-5])
+except Exception:
+	startNum = -1
+
+if (startNum >= 0):
 	if (filepath[-4:] == ".png" or filepath[-4:] == ".jpg"):
 		if (filepath[-4:] == ".png"):
-			filename = filepath[:-8]+"%04d.png"
+			filename = filepath[:-4-c]+"%0"+str(c)+"d.png"
 		else:
-			filename = filepath[:-8]+"%04d.jpg"
-		print (filepath[:-8])
-		sleep(5)
-		subprocess.call(["ffmpeg","-f","image2","-r","12","-start_number",startNum,"-i",filename,"-vcodec","libx264",filepath[:-8]+".mp4", "-y"])
+			filename = filepath[:-4-c]+"%0"+str(c)+"d.png"
+		subprocess.call(["ffmpeg","-f","image2","-r","12","-start_number",str(startNum),"-i",filename,"-vcodec","libx264",filepath[:-8]+".mp4", "-y"])
 		print ("Conversion Completed Successfully! Enjoy your Video :D")
 		sleep(5)
 	else:
