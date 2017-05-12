689fa730d1715746c9c6224faea7876e7fbdd36f
brandoshizzle <cathcart.brandon@gmail.com>
2017-05-12 12:15:45 -0600

    raw_update to prevent \t and \n filepath errors

--- videoEncoder.py
+++ videoEncoder.py
@@ -2,8 +2,8 @@ import sys, os, subprocess
 from time import sleep
 
 print ("Welcome to Byte Camp's Video Encoder.\n")
-filepath = input("Drag-n-Drop a JPG image ending in '0000.jpg'\nAnd press ENTER\n")
-if (filepath[1] == "\"" and filepath[-1] == "\""):
+filepath = raw_input("Drag-n-Drop the first JPG or PNG image ending in '0000.jpg' or similar\nAnd press ENTER to make your video!\n")
+if (filepath[0] == "\"" and filepath[-1] == "\""):
 	filepath = filepath[1:-1]
 print (filepath)
 
@@ -16,6 +16,8 @@ if (startNum > -1):
 			filename = filepath[:-8]+"%04d.png"
 		else:
 			filename = filepath[:-8]+"%04d.jpg"
+		print (filepath[:-8])
+		sleep(5)
 		subprocess.call(["ffmpeg","-f","image2","-r","12","-start_number",startNum,"-i",filename,"-vcodec","libx264",filepath[:-8]+".mp4", "-y"])
 		print ("Conversion Completed Successfully! Enjoy your Video :D")
 		sleep(5)
